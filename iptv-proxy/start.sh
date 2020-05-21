c08d0307e17125686ab2a702d4a26d955f3bfae3
Daniel Salinas <imsplitbit@gmail.com>
2020-05-21 08:25:35 -0500

    use proper spelling of xtream

--- iptv-proxy/start.sh
+++ iptv-proxy/start.sh
@@ -61,18 +61,18 @@ fi
 echo "[info] Proxy host: ${PROXY_HOST}" | ts '%Y-%m-%d %H:%M:%.S'
 
 #echo "[info] Proxy m3u url: ${M3U_URL}" | ts '%Y-%m-%d %H:%M:%.S'
-echo "[debug] xtreme base url: ${XTREME_BASE_URL}" | ts '%Y-%m-%d %H:%M:%.S'
-echo "[debug] xtreme username: ${XTREME_USERNAME}" | ts '%Y-%m-%d %H:%M:%.S'
-echo "[debug] xtreme password: ${XTREME_PASSWORD}" | ts '%Y-%m-%d %H:%M:%.S'
+echo "[debug] xtream base url: ${XTREAM_BASE_URL}" | ts '%Y-%m-%d %H:%M:%.S'
+echo "[debug] xtream username: ${XTREAM_USERNAME}" | ts '%Y-%m-%d %H:%M:%.S'
+echo "[debug] xtream password: ${XTREAM_PASSWORD}" | ts '%Y-%m-%d %H:%M:%.S'
 #echo "[debug] Proxy command: 'iptv-proxy --m3u-url ${M3U_URL} --port $WEB_PORT --user $PROXY_USER --password $PROXY_PASS --hostname $PROXY_HOST --custom-endpoint $PROXY_PATH &'" | ts '%Y-%m-%d %H:%M:%.S'
-echo "[debug] Proxy command: 'iptv-proxy --xtreme-base-url ${XTREME_BASE_URL} --xtreme-user ${XTREME_USERNAME} --xtreme-password ${XTREME_PASSWORD} --user $PROXY_USER --password $PROXY_PASS --hostname $PROXY_HOST --custom-endpoint $PROXY_PATH &" | ts '%Y-%m-%d %H:%M:%.S'
+echo "[debug] Proxy command: 'iptv-proxy --xtream-base-url ${XTREAM_BASE_URL} --xtream-user ${XTREAM_USERNAME} --xtream-password ${XTREAM_PASSWORD} --user $PROXY_USER --password $PROXY_PASS --hostname $PROXY_HOST --custom-endpoint $PROXY_PATH &" | ts '%Y-%m-%d %H:%M:%.S'
 echo "[info] Starting iptv-proxy daemon..." | ts '%Y-%m-%d %H:%M:%.S'
 if [ -z "${PROXY_PATH}" ]
 then
-    su $PUSERNAME -c "iptv-proxy --xtreme-base-url ${XTREME_BASE_URL} --xtreme-user ${XTREME_USERNAME} --xtreme-password ${XTREME_PASSWORD} --port $WEB_PORT --user $PROXY_USER --password $PROXY_PASS --hostname $PROXY_HOST &"
+    su $PUSERNAME -c "iptv-proxy --xtream-base-url ${XTREAM_BASE_URL} --xtream-user ${XTREAM_USERNAME} --xtream-password ${XTREAM_PASSWORD} --port $WEB_PORT --user $PROXY_USER --password $PROXY_PASS --hostname $PROXY_HOST &"
 else
     echo "[info] Proxy path: ${PROXY_PATH}" | ts '%Y-%m-%d %H:%M:%.S'
-    su $PUSERNAME -c "iptv-proxy --xtreme-base-url ${XTREME_BASE_URL} --xtreme-user ${XTREME_USERNAME} --xtreme-password ${XTREME_PASSWORD} --port $WEB_PORT --user $PROXY_USER --password $PROXY_PASS --hostname $PROXY_HOST --custom-endpoint $PROXY_PATH &"
+    su $PUSERNAME -c "iptv-proxy --xtream-base-url ${XTREAM_BASE_URL} --xtream-user ${XTREAM_USERNAME} --xtream-password ${XTREAM_PASSWORD} --port $WEB_PORT --user $PROXY_USER --password $PROXY_PASS --hostname $PROXY_HOST --custom-endpoint $PROXY_PATH &"
 fi
 
 sleep 1
